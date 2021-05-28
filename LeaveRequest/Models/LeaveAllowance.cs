b1f3f46ea33ba191007e58ffe1b8b7df403bc064
heldatrii <heldatriyanti3@gmail.com>
2021-05-28 15:14:13 +0700

    Update LeaveAllowance.cs

--- LeaveRequest/Models/LeaveAllowance.cs
+++ LeaveRequest/Models/LeaveAllowance.cs
@@ -1,13 +1,15 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace LeaveRequest.Models
 {
+    [Table("tb_m_leaveAllowance")]
     public class LeaveAllowance
     {
-        public int Id { get; set; }
+        public int NIK { get; set; }
         public int LeaveAllow { get; set; }
         public int UsedLeaveAllow { get; set; }
     }
