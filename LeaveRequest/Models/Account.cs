0ac153a3eadbce97925f9ed7aa68ec917b7d4970
heldatrii <heldatriyanti3@gmail.com>
2021-05-28 15:48:37 +0700

    commit model account & department

--- LeaveRequest/Models/Account.cs
+++ LeaveRequest/Models/Account.cs
@@ -1,12 +1,17 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace LeaveRequest.Models
 {
+    [Table("tb_m_account")]
     public class Account
     {
+        [Key]
+        [ForeignKey("Person")]
         public int NIK { get; set; }
         public string Password { get; set; }
     }
--- LeaveRequest/Models/Departement.cs
+++ LeaveRequest/Models/Departement.cs
@@ -1,12 +1,16 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace LeaveRequest.Models
 {
+    [Table("tb_m_department")]
     public class Departement
     {
+        [Key]
         public int Id { get; set; }
         public string Name { get; set; }
     }
