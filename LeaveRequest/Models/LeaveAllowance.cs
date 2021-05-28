7e14f187f737b778fd65268ea47ce0d068c9d6c1
mzr2 <mzulfikarabdulaziz@gmail.com>
2021-05-28 15:17:27 +0700

    Revert "Update LeaveAllowance.cs"
    This reverts commit b1f3f46ea33ba191007e58ffe1b8b7df403bc064.

--- LeaveRequest/Models/LeaveAllowance.cs
+++ LeaveRequest/Models/LeaveAllowance.cs
@@ -1,15 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace LeaveRequest.Models
 {
-    [Table("tb_m_leaveAllowance")]
     public class LeaveAllowance
     {
-        public int NIK { get; set; }
+        public int Id { get; set; }
         public int LeaveAllow { get; set; }
         public int UsedLeaveAllow { get; set; }
     }
