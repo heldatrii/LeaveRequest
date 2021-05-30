c22e09af701b30280f1523c4e6c06f6cad8d3f32
heldatrii <heldatriyanti3@gmail.com>
2021-05-30 10:18:38 +0700

    Install Package

---
+++ LeaveRequest/Context/MyContext.cs
@@ -0,0 +1,26 @@
+﻿using LeaveRequest.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LeaveRequest.Context
+{
+    public class MyContext : DbContext
+    {
+        public MyContext(DbContextOptions<MyContext> options) : base(options)
+        { }
+
+        public DbSet<Account> Accounts { get; set; }
+        public DbSet<Departement> Departements { get; set; }
+        public DbSet<LeaveAllowance> LeaveAllowances { get; set; }
+        public DbSet<Person> Persons { get; set; }
+        public DbSet<Request> Requests { get; set; }
+        public DbSet<RequestStatus> RequestStatuses { get; set; }
+        public DbSet<RequestType> RequestTypes { get; set; }
+        public DbSet<Role> Roles { get; set; }
+        public DbSet<RoleAccount> RoleAccounts { get; set; }
+        public DbSet<Tipe> Tipes { get; set; }
+    }
+}
