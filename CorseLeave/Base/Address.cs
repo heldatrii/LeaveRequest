f1256bfbc18de4960d5a944c5c1afeff7be669e0
mzr2 <mzulfikarabdulaziz@gmail.com>
2021-06-02 10:28:49 +0700

    Revert "Revert "CorseLeave""
    This reverts commit 8c136dff5d3d62e16fc997628fb4c31b2bcf1f58.

---
+++ CorseLeave/Base/Address.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CorseLeave.Base
+{
+    public class Address
+    {
+        public string link = "https://localhost:44313/api/";
+    }
+}
