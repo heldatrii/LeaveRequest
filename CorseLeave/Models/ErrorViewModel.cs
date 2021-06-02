e6d0eeb4e760c2784b91bf95af713d27d5587727
heldatrii <heldatriyanti3@gmail.com>
2021-06-02 10:16:24 +0700

    CorseLeave

---
+++ CorseLeave/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace CorseLeave.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
